bce14b8097abe0f7d5113af0a6725c5d315df895
Sharofbek Khudayberdiev <udev21@gmail.com>
2022-04-26 20:12:59 +0500

    copyMessage+

---
+++ method/copy_message.go
@@ -0,0 +1,33 @@
+package method
+
+import (
+	myTypes "github.com/udev-21/golang-tbot-api/method/types"
+	"github.com/udev-21/golang-tbot-api/utils"
+)
+
+func NewCopyMessage(fromChatID string, messageID int64) *CopyMessage {
+	return &CopyMessage{
+		FromChatID: fromChatID,
+		MessageID:  messageID,
+	}
+}
+
+type CopyMessage struct {
+	FromChatID string `json:"from_chat_id"`
+	MessageID  int64  `json:"message_id"`
+
+	myTypes.Captioner
+	myTypes.ParseModer
+	myTypes.DisableNotificationer
+	myTypes.ProtectContenter
+	myTypes.ReplyToMessager
+	myTypes.ReplyMarkuper
+}
+
+func (a *CopyMessage) Endpoint() string {
+	return "copyMessage"
+}
+
+func (a *CopyMessage) Params() (myTypes.Params, error) {
+	return utils.ConvertToMapStringInterface(a)
+}
