ccb338eb8cbacdb80a8c02134b73c1eba7cd8549
Sharofbek Khudayberdiev <udev21@gmail.com>
2022-04-26 08:27:55 +0500

    sendChatAction+

--- method/send_media_group.go
+++ method/send_media_group.go
@@ -7,6 +7,12 @@ import (
 	"github.com/udev-21/golang-tbot-api/utils"
 )
 
+func NewSendMediaGroup(media []myTypes.InputMedia) *MediaGroup {
+	return &MediaGroup{
+		Media: media,
+	}
+}
+
 // https://core.telegram.org/bots/api#sendmediagroup
 type MediaGroup struct {
 	Media []myTypes.InputMedia `json:"media"`
